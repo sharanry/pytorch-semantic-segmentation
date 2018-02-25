a27d4830f3b4465c56b60e22d49ed396df0442f0
Sharan Yalburgi <sharanyalburgi@gmail.com>
2018-02-25 18:34:36 +0530

    fix

--- models/fcn32s.py
+++ models/fcn32s.py
@@ -3,7 +3,7 @@ from torch import nn
 from torchvision import models
 
 from utils import get_upsampling_weight
-from config import vgg16_caffe_path
+from .config import vgg16_caffe_path
 
 
 class FCN32VGG(nn.Module):
